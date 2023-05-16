0900b839efdf16b4785bc94baeb2c3548a42d823
xiayudev <jctuesta7@gmail.com>
2023-05-16 14:02:28 -0700

    fix: incorrect balance factor if node is a leaf

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,24 +34,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	const binary_tree_t *temp = tree;
-	size_t height_l, height_r;
+	size_t height_l = 0, height_r = 0;
 	int b_factor;
 
 	if (temp == NULL)
 		return (0);
 
-	height_l = binary_tree_height(temp->left);
-	height_r = binary_tree_height(temp->right);
-
 	if (temp->left)
 	{
 		if (!temp->left->left && !temp->left->right)
 			height_l++;
+		else
+			height_l += binary_tree_height(temp->left) + 1;
 	}
 	if (temp->right)
 	{
 		if (!temp->right->left && !temp->right->right)
 			height_r++;
+		else
+			height_r += binary_tree_height(temp->right) + 1;
 	}
 
 	b_factor = height_l - height_r;
