f99b9bcc3940646347ddf816612f5667279bd197
xiayudev <jctuesta7@gmail.com>
2023-05-15 19:15:05 -0700

    feat: traverse binary tree in post order

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function to traverse in pre-order
+ * binary_tree_postorder - function to traverse in post-order
  * @tree: Pointer to the root node
  * @func: Pointer to a function call for each node
  *
  * Return: Void
  *
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	const binary_tree_t *temp = tree;
 
 	if (!tree || !func)
 		return;
 
+	binary_tree_postorder(temp->left, func);
+	binary_tree_postorder(temp->right, func);
 	func(temp->n);
-	binary_tree_preorder(temp->left, func);
-	binary_tree_preorder(temp->right, func);
 }
